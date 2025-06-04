6017fe5e260e527fbed2247a4afc0d5d93188e82
HabibMubarakUni <s3091627@stud.uni-frankfurt.de>
2025-06-04 16:04:41 +0200

    Aufgabenteile als Kommentar gekennzeichnet

--- Blatt6/Code/HelloWorld/HelloWorld.cpp
+++ Blatt6/Code/HelloWorld/HelloWorld.cpp
@@ -8,13 +8,17 @@
 
 int main() 
 {
+  // i
   #pragma omp parallel num_threads(10)
   {
     int id = omp_get_thread_num();
-    # pragma omp critical
+
+    # pragma omp critical // ii
     {
       std::cout << " Hello, world! " << id << std::endl;
     }
+
+
   }
 
   return 0;

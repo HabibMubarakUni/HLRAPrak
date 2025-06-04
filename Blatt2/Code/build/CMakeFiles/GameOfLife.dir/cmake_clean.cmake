ddeda4d31fbdf4b9dd886bec3188fadcd7d8ccd0
HabibMubarakUni <s3091627@stud.uni-frankfurt.de>
2025-06-04 21:48:44 +0200

    Blatt 2 hochgeladen

---
+++ Blatt2/Code/build/CMakeFiles/GameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/GameOfLife.cpp.o"
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/GameOfLife.cpp.o.d"
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/World.cpp.o"
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/World.cpp.o.d"
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/cli.cpp.o"
+  "CMakeFiles/GameOfLife.dir/home/users3/lio/s3091627/Documents/HL_Prak_2025/Blatt_2/Aufgabe/Code/cli.cpp.o.d"
+  "GameOfLife"
+  "GameOfLife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
